d3a2a7e8077cb9ae66e42c1db57dd31775b76e07
b.abramov <ba.abrmv@gmail.com>
2021-09-20 00:31:13 +0300

    Some change

--- yolo_camera.py
+++ yolo_camera.py
@@ -1,357 +1,121 @@
-
-
-"""
-Course:  Training YOLO v3 for Objects Detection with Custom Data
-
-Section-2
-Objects Detection in Real Time with YOLO v3 and OpenCV
-File: yolo-3-camera.py
-"""
-
-
-# Detecting Objects in Real Time with OpenCV deep learning library
-#
-# Algorithm:
-# Reading stream video from camera --> Loading YOLO v3 Network -->
-# --> Reading frames in the loop --> Getting blob from the frame -->
-# --> Implementing Forward Pass --> Getting Bounding Boxes -->
-# --> Non-maximum Suppression --> Drawing Bounding Boxes with Labels -->
-# --> Showing processed frames in OpenCV Window
-#
-# Result:
-# Window with Detected Objects, Bounding Boxes and Labels in Real Time
-
-
-# Importing needed libraries
-import numpy as np
-import cv2
-import time
+# from Extensions.originAPI import draw_box_and_labels, get_bounding_box
+from re import T
+from Extensions.TSClassNames import traffic_signs_names
+import copy
 import os
+import time
+from tensorflow.keras.models import load_model
 
+import cv2
+import numpy as np
 
-image_path = os.path.join('test_data', 'traffic-sign-to-test.jpg')
-path_class_names = os.path.join('yolo_detect_data', 'classes.names')
-path_yolo_weights = os.path.join('yolo_detect_data', 'yolov3_ts_train_8500.weights')
-path_model = os.path.join(os.getcwd(), 'yolo_detect_data', 'model_tr.h5')
-path_cfg = os.path.join('yolo_detect_data', 'yolov3_ts_test.cfg')
-path_data = os.path.join('yolo_detect_data', 'ts_data.data')
-probability_minimum = 0.4
-threshold = 0.4
+from Extensions.netAPI import netAPI, Convert
+
+net = netAPI()
+video_resource = '192.168.1.10:4747/video'
+
+net.path_class_names = os.path.join(os.getcwd(), 'yolo_detect_data', 'classes.names')
+net.path_yolo_weights = os.path.join(os.getcwd(), 'yolo_detect_data', 'yolov3_ts_train_8500.weights')
+net.path_model_cnn = os.path.join(os.getcwd(), 'yolo_detect_data', 'model_tr.h5')
+net.path_cfg = os.path.join(os.getcwd(), 'yolo_detect_data', 'yolov3_ts_test.cfg')
+net.path_data = os.path.join(os.getcwd(), 'yolo_detect_data', 'ts_data.data')
+net.yolo_probability_minimum = 0.4
+net.yolo_threshold = 0.4
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+path_model_tf = net.path_model_cnn
 
+path_csv_tf = os.path.join(os.getcwd(), 'yolo_detect_data', 'labels.csv')
 
-"""
-Start of:
-Reading stream video from camera
-"""
-frameWidth = 640         # CAMERA RESOLUTION
+frameWidth = 640 
 frameHeight = 480
 brightness = 180
-threshold = 0.75         # PROBABLITY THRESHOLD
-# Defining 'VideoCapture' object
-# and reading stream video from camera
-camera = cv2.VideoCapture(0)
+threshold = 0.75 
+
+camera = cv2.VideoCapture('http://192.168.1.10:4747/video')
 camera.set(3, frameWidth)
 camera.set(4, frameHeight)
 camera.set(10, brightness)
-# camera
 
-# Preparing variables for spatial dimensions of the frames
 h, w = None, None
 
-"""
-End of:
-Reading stream video from camera
-"""
+labels = net.get_labels(net.path_class_names)
+yolo_net, layers_all, layers_names_out, col = net.load_yolo_network(net.path_cfg, net.path_yolo_weights, labels)
+tf_model = load_model(path_model_tf)  # cv2.dnn.readNetFromTensorflow(net.path_model_cnn, path_csv_tf)
 
+start = time.time()
 
-"""
-Start of:
-Loading YOLO v3 network
-"""
-
-# Loading COCO class labels from file
-# Opening file
-# Pay attention! If you're using Windows, yours path might looks like:
-# r'yolo-coco-data\coco.names'
-# or:
-# 'yolo-coco-data\\coco.names'
-with open(path_class_names) as f:
-    # Getting labels reading every line
-    # and putting them into the list
-    labels = [line.strip() for line in f]
-
-
-# # Check point
-# print('List with labels names:')
-# print(labels)
-
-# Loading trained YOLO v3 Objects Detector
-# with the help of 'dnn' library from OpenCV
-# Pay attention! If you're using Windows, yours paths might look like:
-# r'yolo-coco-data\yolov3.cfg'
-# r'yolo-coco-data\yolov3.weights'
-# or:
-# 'yolo-coco-data\\yolov3.cfg'
-# 'yolo-coco-data\\yolov3.weights'
-network = cv2.dnn.readNetFromDarknet(path_cfg, path_yolo_weights)
-
-# Getting list with names of all layers from YOLO v3 network
-layers_names_all = network.getLayerNames()
-
-# # Check point
-# print()
-# print(layers_names_all)
-
-# Getting only output layers' names that we need from YOLO v3 algorithm
-# with function that returns indexes of layers with unconnected outputs
-layers_names_output = \
-    [layers_names_all[i[0] - 1] for i in network.getUnconnectedOutLayers()]
-
-# # Check point
-# print()
-# print(layers_names_output)  # ['yolo_82', 'yolo_94', 'yolo_106']
-
-# Setting minimum probability to eliminate weak predictions
-probability_minimum = 0.5
-
-# Setting threshold for filtering weak bounding boxes
-# with non-maximum suppression
-threshold = 0.3
-
-# Generating colours for representing every detected object
-# with function randint(low, high=None, size=None, dtype='l')
-colours = np.random.randint(0, 255, size=(len(labels), 3), dtype='uint8')
-
-# # Check point
-# print()
-# print(type(colours))  # <class 'numpy.ndarray'>
-# print(colours.shape)  # (80, 3)
-# print(colours[0])  # [172  10 127]
+f = 0
 
 """
-End of:
-Loading YOLO v3 network
+Start detect in loop
 """
-
-
-"""
-Start of:
-Reading frames in the loop
-"""
-
-# Defining loop for catching frames
 while True:
-    # Capturing frame-by-frame from camera
     _, frame = camera.read()
-
-    # Getting spatial dimensions of the frame
-    # we do it only once from the very beginning
-    # all other frames have the same dimension
+    start_detect = time.time()
     if w is None or h is None:
         # Slicing from tuple only first two elements
         h, w = frame.shape[:2]
+    crop_frame = copy.deepcopy(frame)
+    yolo_blob = net.get_blob(frame)
+    output_from_network = net.forward_pass(yolo_net, yolo_blob, layers_names_out)
+    bounding_boxes, confidences, class_numbers = net.get_bounding_box(net.yolo_probability_minimum, 
+                                                                        frame, 
+                                                                        output_from_network)
+    res_suppression = net.non_max_suppression(bounding_boxes, 
+                                                confidences, 
+                                                net.yolo_probability_minimum, 
+                                                net.yolo_threshold)
+    img_res, data_ts, counter = net.draw_box_and_labels(res_suppression, 
+                                                        frame, 
+                                                        labels, 
+                                                        class_numbers, 
+                                                        bounding_boxes, 
+                                                        col, 
+                                                        confidences)
+    ts = 0
+    if len(data_ts) > 0:
+        x_min, x_max, y_min, y_max = data_ts[0]['coord_box']
+        img_crop = crop_frame[y_min:y_max, x_min:x_max]
+        img_crop_base = copy.deepcopy(img_crop)
+        # tf_blob = net.get_blob(img_crop)
 
-    """
-    Start of:
-    Getting blob from current frame
-    """
+        img = np.asarray(img_crop_base)
+        img = cv2.resize(img, (32, 32))
+        img = Convert.preprocessing(img)
+        img = img.reshape(1, 32, 32, 1)
 
-    # Getting blob from current frame
-    # The 'cv2.dnn.blobFromImage' function returns 4-dimensional blob from current
-    # frame after mean subtraction, normalizing, and RB channels swapping
-    # Resulted shape has number of frames, number of channels, width and height
-    # E.G.:
-    # blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size, mean, swapRB=True)
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-                                 swapRB=True, crop=False)
+        prediction = tf_model.predict(img)
+        class_index = np.argmax(prediction, axis=1)
+        probability_value = np.amax(prediction)
 
-    """
-    End of:
-    Getting blob from current frame
-    """
+        if probability_value > threshold:
+            ts = traffic_signs_names(class_index)
+            cv2.putText(frame, f"Type of sign: {ts}", (0, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame, str(round(probability_value*100, 2))+"%", (60, 75), font, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
 
-    """
-    Start of:
-    Implementing Forward pass
-    """
+    print()
+    if len(bounding_boxes) !=0:
+        print('Total objects been detected:', len(bounding_boxes))
+        print('Number of objects left after non-maximum suppression:', counter - 1)
+        if ts != 0:
+            print(ts)
 
-    # Implementing forward pass with our blob and only through output layers
-    # Calculating at the same time, needed time for forward pass
-    network.setInput(blob)  # setting blob as input to the network
-    start = time.time()
-    output_from_network = network.forward(layers_names_output)
-    end = time.time()
-
-    # Showing spent time for single current frame
-    print('Current frame took {:.5f} seconds'.format(end - start))
-
-    """
-    End of:
-    Implementing Forward pass
-    """
-
-    """
-    Start of:
-    Getting bounding boxes
-    """
-
-    # Preparing lists for detected bounding boxes,
-    # obtained confidences and class's number
-    bounding_boxes = []
-    confidences = []
-    class_numbers = []
-
-    # Going through all output layers after feed forward pass
-    for result in output_from_network:
-        # Going through all detections from current output layer
-        for detected_objects in result:
-            # Getting 80 classes' probabilities for current detected object
-            scores = detected_objects[5:]
-            # Getting index of the class with the maximum value of probability
-            class_current = np.argmax(scores)
-            # Getting value of probability for defined class
-            confidence_current = scores[class_current]
-
-            # # Check point
-            # # Every 'detected_objects' numpy array has first 4 numbers with
-            # # bounding box coordinates and rest 80 with probabilities
-            # # for every class
-            # print(detected_objects.shape)  # (85,)
-
-            # Eliminating weak predictions with minimum probability
-            if confidence_current > probability_minimum:
-                # Scaling bounding box coordinates to the initial frame size
-                # YOLO data format keeps coordinates for center of bounding box
-                # and its current width and height
-                # That is why we can just multiply them elementwise
-                # to the width and height
-                # of the original frame and in this way get coordinates for center
-                # of bounding box, its width and height for original frame
-                box_current = detected_objects[0:4] * np.array([w, h, w, h])
-
-                # Now, from YOLO data format, we can get top left corner coordinates
-                # that are x_min and y_min
-                x_center, y_center, box_width, box_height = box_current
-                x_min = int(x_center - (box_width / 2))
-                y_min = int(y_center - (box_height / 2))
-
-                # Adding results into prepared lists
-                bounding_boxes.append([x_min, y_min,
-                                       int(box_width), int(box_height)])
-                confidences.append(float(confidence_current))
-                class_numbers.append(class_current)
-
-    """
-    End of:
-    Getting bounding boxes
-    """
-
-    """
-    Start of:
-    Non-maximum suppression
-    """
-
-    # Implementing non-maximum suppression of given bounding boxes
-    # With this technique we exclude some of bounding boxes if their
-    # corresponding confidences are low or there is another
-    # bounding box for this region with higher confidence
-
-    # It is needed to make sure that data type of the boxes is 'int'
-    # and data type of the confidences is 'float'
-    # https://github.com/opencv/opencv/issues/12789
-    results = cv2.dnn.NMSBoxes(bounding_boxes, confidences,
-                               probability_minimum, threshold)
-
-    """
-    End of:
-    Non-maximum suppression
-    """
-
-    """
-    Start of:
-    Drawing bounding boxes and labels
-    """
-
-    # Checking if there is at least one detected object
-    # after non-maximum suppression
-    if len(results) > 0:
-        # Going through indexes of results
-        for i in results.flatten():
-            # Getting current bounding box coordinates,
-            # its width and height
-            x_min, y_min = bounding_boxes[i][0], bounding_boxes[i][1]
-            box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
-
-            # Preparing colour for current bounding box
-            # and converting from numpy array to list
-            colour_box_current = colours[class_numbers[i]].tolist()
-
-            # # # Check point
-            # print(type(colour_box_current))  # <class 'list'>
-            # print(colour_box_current)  # [172 , 10, 127]
-
-            # Drawing bounding box on the original current frame
-            cv2.rectangle(frame, (x_min, y_min),
-                          (x_min + box_width, y_min + box_height),
-                          colour_box_current, 2)
-
-            # Preparing text with label and confidence for current bounding box
-            text_box_current = '{}: {:.4f}'.format(labels[int(class_numbers[i])],
-                                                   confidences[i])
-
-            # Putting text with label and confidence on the original image
-            cv2.putText(frame, text_box_current, (x_min, y_min - 5),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour_box_current, 2)
-
-    """
-    End of:
-    Drawing bounding boxes and labels
-    """
-
-    """
-    Start of:
-    Showing processed frames in OpenCV Window
-    """
-
-    # Showing results obtained from camera in Real Time
-
-    # Showing current frame with detected objects
-    # Giving name to the window with current frame
-    # And specifying that window is resizable
+    if ((time.time() - start) >= 1):
+        print(f'FPS: {f}')
+        f = 0
+        start = time.time()
+    print(f'Time detect: {time.time() - start_detect}')
     cv2.namedWindow('YOLO v3 Real Time Detections', cv2.WINDOW_NORMAL)
     # Pay attention! 'cv2.imshow' takes images in BGR format
     cv2.imshow('YOLO v3 Real Time Detections', frame)
 
-    # Breaking the loop if 'q' is pressed
+    f += 1
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-    """
-    End of:
-    Showing processed frames in OpenCV Window
-    """
-
-"""
-End of:
-Reading frames in the loop
-"""
-
-
-# Releasing camera
+    # Releasing camera
 camera.release()
 # Destroying all opened OpenCV windows
-cv2.destroyAllWindows()
-
-
-"""
-Some comments
-
-cv2.VideoCapture(0)
-
-To capture video, it is needed to create VideoCapture object.
-Its argument can be camera's index or name of video file.
-Camera index is usually 0 for built-in one.
-Try to select other cameras by passing 1, 2, 3, etc.
-"""
+cv2.destroyAllWindows()   
+    
